bf8bcaa58df392eb60239659cb27548b255f90f3
Mehmet Ali AKAR <maliakar123@gmail.com>
2024-04-20 12:32:38 +0300

    Add project files.

---
+++ ASPNetCoreWebApiDemo_1/Models/EmpContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNetCoreWebApiDemo_1.Models
+{
+    public class EmpContext:DbContext
+    {
+        public EmpContext(DbContextOptions options) : base(options) {}
+            DbSet <Employees> Employees{
+                get;
+                set;    
+            }
+    }
+}
---
+++ ASPNetCoreWebApiDemo_1/Models/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetCoreWebApiDemo_1.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public decimal Salary { get; set; }
+        public string Designation { get; set; }
+
+    }
+}
